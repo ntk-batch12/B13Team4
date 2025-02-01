c89819be85f3bfc1e58c89812e072a941c32bc3f
hayde <hayde@Hayder.attlocal.net>
2025-01-31 20:37:26 -0600

    add a and b

--- src/ydemo/YDemoTest.java
+++ src/ydemo/YDemoTest.java
@@ -4,6 +4,10 @@ public class YDemoTest {
 	
 	public static void main(String[] args) {
 		System.out.println("Team 4");
+		
+	}
+	public void add(int a,int b) {
+		System.out.println(a+b);
 	}
 	
 }
