78d33163c1ab7e039f380f2fea0b89f591513973
lenovo <lenovo@LAPTOP-MJ4J3446>
2025-02-01 08:11:26 -0600

    I added a push method with a comment.

--- src/ydemo/YDemoTest.java
+++ src/ydemo/YDemoTest.java
@@ -1,35 +1,59 @@
 package ydemo;
 
 public class YDemoTest {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Team 4");
 		System.out.println("Team4IsTheBest");
 	}
-	public void add(int a,int b) {
-		System.out.println(a+b);
+
+	public void add(int a, int b) {
+		System.out.println(a + b);
 	}
-	
+
 	public void myName() {
 		System.out.println("David Avila");
 	}
-	public void getInfo(){
+
+	public void getInfo() {
 		System.out.println("Brett");
 	}
-	//add a comment 2
-	
-	//best work team
-	
-	//Just a comment to pull
-	
-	//Noor your code can be below
-	
+	// add a comment 2
 
- public void subtract(int y,int z) {
-	 System.out.println(y-z);
+	// best work team
 
-}
- public void divide(int a , int b) {
-	 System.out.println(a/b);
- }
+	// Just a comment to pull
+
+	// Noor your code can be below
+
+	public void subtract(int y, int z) {
+		System.out.println(y - z);
+
+	}
+
+	public void divide(int a, int b) {
+		System.out.println(a / b);
+	}
+	
+	public void myFirstPush() {
+		System.out.println("This is my first push Team4");	
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
