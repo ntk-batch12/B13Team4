808ce00afa639bd0c80a8dd9bedb2548fa2c3570
RAFEEF <RAFEEF@10.0.0.65>
2025-01-31 21:44:04 -0500

    add

--- src/ydemo/YDemoTest.java
+++ src/ydemo/YDemoTest.java
@@ -4,7 +4,7 @@ public class YDemoTest {
 	
 	public static void main(String[] args) {
 		System.out.println("Team 4");
-		
+		System.out.println("Team4IsTheBest");
 	}
 	public void add(int a,int b) {
 		System.out.println(a+b);
